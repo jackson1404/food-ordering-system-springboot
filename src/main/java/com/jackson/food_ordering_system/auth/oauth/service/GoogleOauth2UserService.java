f2e8462cbbfe1e55c4fc009101e9fc1aa4b1f7c4
jackson1404 <jackson14042004@gmail.com>
2025-08-27 15:17:09 +0630

    added oauth folder

--- src/main/java/com/jackson/food_ordering_system/auth/oauth/service/GoogleOauth2UserService.java
+++ src/main/java/com/jackson/food_ordering_system/auth/oauth/service/GoogleOauth2UserService.java
@@ -4,7 +4,7 @@
  * Version      : 	
  * History  :	
  * *************************************************************/
-package com.jackson.food_ordering_system.auth.oauth2.service;
+package com.jackson.food_ordering_system.auth.oauth.service;
 
 import com.jackson.food_ordering_system.user.model.UserEntity;
 import com.jackson.food_ordering_system.user.repo.UserRepository;
